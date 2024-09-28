c901b007039e4f1dcac62448a1c3dc7e0ebf25b7
DinoMint <dinomint@192.168.101.7>
2024-09-28 13:48:46 +0700

    add click point and energy temp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components'
-import { telegramInitDataRawToObject } from '@/utils'
+import { formatNumberDownRound, telegramInitDataRawToObject } from '@/utils'
 import { retrieveLaunchParams } from '@telegram-apps/sdk'
 import {
   Modal,
@@ -7,7 +7,8 @@ import {
   Progress,
   Spinner
 } from '@telegram-apps/telegram-ui'
-import { useEffect, useState, type FC } from 'react'
+import { get } from 'lodash'
+import { useEffect, useRef, useState, type FC } from 'react'
 import { Link } from 'react-router-dom'
 
 // Define UserInfo
@@ -58,22 +59,26 @@ const TABS = [
 const PLAY_IMAGE = '/assets/images/play.gif'
 
 export const IndexPage: FC = () => {
-  const [userInfo, setUserInfo] = useState<UserInfo | undefined>(undefined)
+  const interval = useRef<any>()
+  const [userInfo, setUserInfo] = useState<UserInfo | undefined | any>(
+    undefined
+  )
   const [error, setError] = useState('')
   const [currentTab, setCurrentTab] = useState(TABS[0].id)
 
   const [playImage, setPlayImage] = useState(PLAY_IMAGE)
+  const [totalBalance, setTotalBalance] = useState<number>(0)
+  const [energy, setEnergy] = useState<number>(0)
 
   // Get Telegram init raw data
   const { initDataRaw } = retrieveLaunchParams()
   const teleUser = telegramInitDataRawToObject(initDataRaw ?? '')
-  console.log(teleUser)
 
   // Define header config
   const headerConfig: Partial<RequestInit> = {
     headers: {
-      // Authorization: `tma query_id=AAFCficsAAAAAEJ-JywGIiU5&user=%7B%22id%22%3A740785730%2C%22first_name%22%3A%22Tho%E1%BA%A1i%22%2C%22last_name%22%3A%22Nguyen%22%2C%22username%22%3A%22roster90%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1727479557&hash=240e2bedd123218206ba3d728f044cb4be02fdaebc109f3dc82db33832b5dd8e`,
-      Authorization: `tma ${initDataRaw}`,
+      Authorization: `tma query_id=AAFCficsAAAAAEJ-JywGIiU5&user=%7B%22id%22%3A740785730%2C%22first_name%22%3A%22Tho%E1%BA%A1i%22%2C%22last_name%22%3A%22Nguyen%22%2C%22username%22%3A%22roster90%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1727479557&hash=240e2bedd123218206ba3d728f044cb4be02fdaebc109f3dc82db33832b5dd8e`,
+      // Authorization: `tma ${initDataRaw}`,
       'Content-Type': 'application/json'
     }
   }
@@ -104,6 +109,37 @@ export const IndexPage: FC = () => {
   }
 
   useEffect(() => {
+    if (userInfo) {
+      setTotalBalance(get(userInfo, 'balance', 0))
+      setEnergy(get(userInfo, 'incubationCanSpent', 0))
+      interval.current = setInterval(() => {
+        fetch(API_GET_INFO, {
+          method: 'GET',
+          ...headerConfig
+        })
+          .then(async (res) => {
+            // Set user info
+            if (res.status === 200) {
+              const userData = await res.json()
+              console.log('asahsas', userData.data)
+              setUserInfo(userData.data)
+            }
+          })
+          .catch((err) => {
+            console.log(err)
+            // TODO
+            setError(err.toString())
+          })
+      }, 5000)
+    }
+    return () => {
+      if (interval.current) {
+        clearInterval(interval.current)
+      }
+    }
+  }, [userInfo])
+
+  useEffect(() => {
     async function initPage() {
       // Call api get user info
       fetch(API_GET_INFO, {
@@ -112,7 +148,6 @@ export const IndexPage: FC = () => {
       })
         .then(async (res) => {
           // Create new account
-          console.log(res)
           if (res.status === 403) {
             await createNewAccount()
             // TODO
@@ -125,7 +160,7 @@ export const IndexPage: FC = () => {
             if (!userData.data) {
               await createNewAccount()
             } else {
-              setUserInfo(userData.data as UserInfo)
+              setUserInfo(userData.data)
             }
           }
 
@@ -148,13 +183,19 @@ export const IndexPage: FC = () => {
   const handleClick = async () => {
     // Start animation when tap
     setPlayImage(`${PLAY_IMAGE}?v=${new Date().getTime()}`)
-
-    // Call api to update point
-    const res = await fetch(API_PLAY, {
-      method: 'PUT',
-      ...headerConfig
-    })
-    console.log(res)
+    if (energy > 0) {
+      // Call api to update point
+      const res = await fetch(API_PLAY, {
+        method: 'PUT',
+        ...headerConfig
+      })
+      setTotalBalance((prev) => prev + 1)
+      console.log(res)
+      setEnergy((prev: number) => {
+        if (prev <= 0) return prev
+        return prev - 1
+      })
+    }
   }
 
   /**
@@ -180,7 +221,7 @@ export const IndexPage: FC = () => {
         <div className='inner'>
           <div className='point'>
             <img src='/assets/images/coin.png' alt='' />
-            <span>109 391</span>
+            <span>{formatNumberDownRound(totalBalance)}</span>
           </div>
           <div className='progress'>
             <img
@@ -193,11 +234,11 @@ export const IndexPage: FC = () => {
           <div className='info'>
             <div className='level'>
               <img src='/assets/images/level.png' alt='' />
-              <span>3</span>
+              <span>{get(userInfo, 'level', 1)}</span>
             </div>
             <div className='tap'>
               <img src='/assets/images/tap.png' alt='' />
-              <span>Tap: 123</span>
+              <span>Tap: {formatNumberDownRound(totalBalance)}</span>
             </div>
           </div>
         </div>
@@ -216,7 +257,7 @@ export const IndexPage: FC = () => {
           <Button size='sm' variant='warning' className='tap'>
             <div>
               <img src='/assets/images/flash.png' height={20} alt='' />
-              200/600
+              {energy}/600
             </div>
           </Button>
         </div>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { BigNumber } from 'bignumber.js'
+
 export const telegramInitDataRawToObject = (encodedString: string) => {
   // Decode the URL-encoded string
   const decodedString = decodeURIComponent(encodedString)
@@ -17,4 +19,25 @@ export const telegramInitDataRawToObject = (encodedString: string) => {
     }
   })
   return result
+}
+
+export const formatNumberDownRound = (
+  number: number | null | undefined,
+  decimal = 3,
+  suffixes = ''
+) => {
+  if (number === null || number === undefined) return 0
+  const bigValue = new BigNumber(number)
+  const bigValueFormatted = bigValue.toFormat(decimal, BigNumber.ROUND_DOWN, {
+    decimalSeparator: '.',
+    groupSeparator: ',',
+    groupSize: 3
+  })
+  if (bigValueFormatted === 'NaN') {
+    return `0 ${suffixes}`
+  }
+  // remove zero and dot in decimal part (ex: 1,111.00 => 1,111)
+  const valueConcated = bigValueFormatted.replace(/\.?0+$/, '')
+
+  return `${valueConcated} ${suffixes}`
 }
