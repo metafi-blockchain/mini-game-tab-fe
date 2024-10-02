2180825946777c4dc5d46712f19fdd20d9cdb4d4
DinoMint <dinomint@192.168.101.7>
2024-10-02 21:31:39 +0700

    update env

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -9,7 +9,7 @@ import { ACCESS_TOKEN, EXPIRE_TIME } from '@/constants';
 import { useInitData, useLaunchParams } from '@tma.js/sdk-react';
 import { useUser } from '@/contexts/UserContext';
 let queryId =
-	'query_id=AAG77WkbAAAAALvtaRsfJuX8&user=%7B%22id%22%3A459926971%2C%22first_name%22%3A%22Shaky%22%2C%22last_name%22%3A%22James%22%2C%22username%22%3A%22jsmile1994%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1722932001&hash=857c33e878283ac3c848d04c19ae1604a52760da0db44726e946abb177b4a134';
+	'query_id=AAG77WkbAAAAALvtaRtj__lT&user=%7B%22id%22%3A459926971%2C%22first_name%22%3A%22Shaky%22%2C%22last_name%22%3A%22James%22%2C%22username%22%3A%22jsmile1994%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1727878320&hash=8cc7fec68b54bd3ce16402bdcd714a8ccaaddde52d20692737b954ce76f1d84e';
 const OkLoadingPage = () => {
 	const navigate = useNavigate();
 	const initDataRaw = useLaunchParams().initDataRaw;
