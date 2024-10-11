5fe2bc5d99ff90a5373a22d25b4b4866c85952e2
dev-thanhnx <dev.thanhnx@gmail.com>
2024-10-12 02:14:43 +0700

    ui tap

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -9,7 +9,7 @@ import { ACCESS_TOKEN, EXPIRE_TIME } from '@/constants';
 import { useInitData, useLaunchParams } from '@tma.js/sdk-react';
 import { useUser } from '@/contexts/UserContext';
 let queryId =
-	'query_id=AAG77WkbAAAAALvtaRtj__lT&user=%7B%22id%22%3A459926971%2C%22first_name%22%3A%22Shaky%22%2C%22last_name%22%3A%22James%22%2C%22username%22%3A%22jsmile1994%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1727878320&hash=8cc7fec68b54bd3ce16402bdcd714a8ccaaddde52d20692737b954ce76f1d84e';
+	'query_id=AAH-c2R7AAAAAP5zZHufxuy0&user=%7B%22id%22%3A2070180862%2C%22first_name%22%3A%22Th%C3%A0nh%22%2C%22last_name%22%3A%22Nguy%E1%BB%85n%20Xu%C3%A2n%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1727887718&hash=72c2da49a461f83d5d4f94891d2a5e5d00b09832adb34405c2425b48cf775e99';
 const OkLoadingPage = () => {
 	const navigate = useNavigate();
 	const initDataRaw = useLaunchParams().initDataRaw;
