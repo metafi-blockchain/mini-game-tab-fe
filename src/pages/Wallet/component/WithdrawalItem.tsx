a60d759aa32ba53766d0e3f086a2de24fe8b2238
oht <nguyenthanhdat.dcna@gmail.com>
2024-10-22 12:58:08 +0700

    Update UI responsive

--- src/pages/Wallet/component/WithdrawalItem.tsx
+++ src/pages/Wallet/component/WithdrawalItem.tsx
@@ -1,6 +1,5 @@
-import OKButton from '@/components/Button';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { formatNumberDownRound, minimizeAddress } from '@/helpers';
-import { ReactNode } from 'react';
 import './index.scss';
 
 export interface IPropWithdrawalItem {
@@ -13,56 +12,50 @@ export interface IPropWithdrawalItem {
 	isDiff?: boolean;
 }
 const WithdrawalItem = (props: IPropWithdrawalItem) => {
-	const { name, wallet, address, time, amount, status, isDiff = false } = props;
+	const { name, amount, status, isDiff = false } = props;
 	return (
 		<div className="flex flex-row gap-3 mt-2 item-booster items-center bg-card h-[67px]">
 			<div className="flex flex-1 flex-row justify-between">
 				<div className="flex flex-1 flex-col">
 					<div className="flex flex-col gap-1">
 						{isDiff ? (
-							<div className="flex flex-1 flex-row justify-between">
-								<div>
+							<div className="withdrawal-item-header">
+								<div className="withdrawal-item-name">
 									<span className="text-sm font-medium">{name}</span>
 								</div>
-								<div
-									className="flex flex-1 flex-row"
-									style={{ justifyContent: 'flex-end' }}
-								>
-									<div className="flex flex-row gap-1 items-center">
-										<img
-											src="/images/icons/ton-logo.svg"
-											width={16}
-											height={16}
-											alt="ton-coin"
-										/>
-										<span className="text-sm font-medium text-white">
-											{formatNumberDownRound(amount)}
-										</span>
-									</div>
+								<div className="withdrawal-item-value">
+									<img
+										src="/images/icons/ton-logo.svg"
+										width={16}
+										height={16}
+										alt="ton-coin"
+									/>
+									<span className="text-sm font-medium text-white">
+										{formatNumberDownRound(amount)}
+									</span>
 								</div>
 							</div>
 						) : (
-							<div className="flex flex-1 flex-row justify-between">
-								<div className="flex flex-1 flex-row ">
-									<div className="flex flex-row gap-1 items-center">
-										<img
-											src="/images/icons/ton-logo.svg"
-											width={16}
-											height={16}
-											alt="ton-coin"
-										/>
-										<span className="text-sm font-medium text-white">
-											{formatNumberDownRound(amount)}
-										</span>
-									</div>
+							<div className="withdrawal-item-header">
+								<div className="withdrawal-item-value">
+									<img
+										src="/images/icons/ton-logo.svg"
+										width={16}
+										height={16}
+										alt="ton-coin"
+									/>
+									<span className="text-sm font-medium text-white">
+										{formatNumberDownRound(amount)}
+									</span>
 								</div>
 								<div>
-									<span className="text-sm">{status}</span>
+									<span className={`withdrawal-item-name ${status}`}>{status}</span>
 								</div>
 							</div>
 						)}
 
 						<a
+							rel="noreferrer"
 							href={`${
 								import.meta.env.VITE_TON_SCAN
 							}/${'UQDBhNgJzBcwQDCVN_QZIijMTQD6FaNyLOHiZGl6t6Zqz1X5'}`}
