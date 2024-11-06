a50c07e4382c2539a14a281a99aab7d025008704
Rooster <nguyenthoai06011990@gmail.com>
2024-11-06 11:16:27 +0700

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 
 
-docker buildx build --platform linux/amd64 -t metafi/ok-mini-app-fe:0.1.1./ --load .
+docker buildx build --platform linux/amd64 -t metafi/ok-mini-app-fe:0.1.2  --load .
 
-docker buildx build --platform linux/amd64,linux/arm64 -t metafi/ok-mini-app-fe:0.1.1 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t metafi/ok-mini-app-fe:0.1.2 --push .
 
-docker push metafi/ok-mini-app-fe:0.1.1
+docker push metafi/ok-mini-app-fe:0.1.2
